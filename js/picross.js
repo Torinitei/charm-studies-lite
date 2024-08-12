d9b677a26025ec4c1800409433a46e8535a82f91
Torinitei <123199498+Torinitei@users.noreply.github.com>
2024-08-12 16:16:15 +0200

    i gotta stop leaving the dev test in i'm sorry

--- js/picross.js
+++ js/picross.js
@@ -1305,7 +1305,7 @@ console.log(allhintsY)
 
 			let hintsX = this.getHintsX(solution);
 			let hintsY = this.getHintsY(solution);
-			// state = solution; // DEV TEST
+			// state = solution; // DEV TEST (cries)
 
 			this.set({
 				state: state,
